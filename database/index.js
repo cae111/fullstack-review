b20cea8c8f3a198dfb5d3c66f4a9894b0acbd38c
Eunbyul Cho <hello@eunbyulcho.me>
2018-05-25 16:22:48 -0700

    posting data to database

--- database/index.js
+++ database/index.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
-
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
   name:String,
   userName:String,
   decription:String,
   url:String,
-  unique: true
+  forks:Number,
+
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
@@ -16,9 +18,11 @@ let save = (userRepo) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
-  product.save().then(function(product) {
-    console.log('saved to database' + userRepo);
+  let repo = new Repo(userRepo);
+  repo.save().then(function(product) {
+    console.log('saved to database', product);
  });
 }
 
 module.exports.save = save;
+
